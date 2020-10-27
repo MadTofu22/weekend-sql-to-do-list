94f6d38cc990d11cf5d5dfb042e9d691b023dfc2
MadTofu22 <stutler.tom@gmail.com>
2020-10-27 10:54:54 -0500

    updated db info

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -3,8 +3,8 @@ const pg = require('pg');
 // Setup PG to connect to DB
 const Pool = pg.Pool;
 const pool = new Pool ({
-    database: 't2w3_challenge', 
-    host: 'localhost', 
+    database: 'da26smnnj4j2tg',
+    host: 'ec2-54-160-18-230.compute-1.amazonaws.com', 
     port: 5432, 
     max: 10, 
     idleTimeoutMillis: 30000 
